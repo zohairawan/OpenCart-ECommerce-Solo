76f82f704595ec4437be68e8a1581677f18faae9
zohairawan <35776095+zohairawan@users.noreply.github.com>
2025-05-06 06:23:16 -0700

    Delete unused imports

--- src/test/java/tests/TC001_AccountRegistrationTest.java
+++ src/test/java/tests/TC001_AccountRegistrationTest.java
@@ -1,7 +1,5 @@
 package tests;
 
-import org.apache.commons.lang3.RandomStringUtils;
-import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.AccountRegistrationPage;
