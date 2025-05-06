b81af0eb70d9ced011d63bdd02c6092b58e4b856
zohairawan <35776095+zohairawan@users.noreply.github.com>
2025-05-06 04:18:44 -0700

    Add WebDriverWait to BasePage

--- src/test/java/pages/base/BasePage.java
+++ src/test/java/pages/base/BasePage.java
@@ -2,13 +2,18 @@ package pages.base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class BasePage {
 
     public WebDriver driver;
+    public WebDriverWait wait;
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver,this);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
     }
 }
