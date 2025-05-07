e04fc994c5bc9af06c95d3c4740aa550d682dcf4
zohairawan <35776095+zohairawan@users.noreply.github.com>
2025-05-07 01:46:40 -0700

    Add cross browser and parallel testing functionality

--- src/test/java/tests/base/BaseTest.java
+++ src/test/java/tests/base/BaseTest.java
@@ -5,8 +5,11 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 
 public class BaseTest {
 
@@ -14,9 +17,15 @@ public class BaseTest {
     public Logger logger;
 
     @BeforeClass
-    public void setup() {
+    @Parameters({"os","browser"})
+    public void setup(String os, String browser) {
         logger = LogManager.getLogger(this.getClass()); // Loads log4j2.xml file
-        driver = new ChromeDriver();
+        switch (browser.toLowerCase()) {
+            case "chrome" -> driver = new ChromeDriver();
+            case "firefox" -> driver = new FirefoxDriver();
+            case "edge" -> driver = new EdgeDriver();
+            default -> {logger.error("Invalid browser name");return;}
+        }
         driver.manage().deleteAllCookies();
         driver.manage().window().maximize();
         driver.get("https://tutorialsninja.com/demo/");
