868564e86e3c789234fdd9f46dc729609a0c23c2
zohairawan <35776095+zohairawan@users.noreply.github.com>
2025-05-07 04:33:48 -0700

    Add functionality to read URL from config.properties file

--- src/test/java/tests/base/BaseTest.java
+++ src/test/java/tests/base/BaseTest.java
@@ -11,24 +11,38 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
 public class BaseTest {
 
     public WebDriver driver;
     public Logger logger;
+    public Properties propertiesFile;
 
     @BeforeClass
-    @Parameters({"os","browser"})
+    @Parameters({"os", "browser"})
     public void setup(String os, String browser) {
-        logger = LogManager.getLogger(this.getClass()); // Loads log4j2.xml file
-        switch (browser.toLowerCase()) {
-            case "chrome" -> driver = new ChromeDriver();
-            case "firefox" -> driver = new FirefoxDriver();
-            case "edge" -> driver = new EdgeDriver();
-            default -> {logger.error("Invalid browser name");return;}
+        try (FileReader fileReader = new FileReader(System.getProperty("user.dir") + "//src//test//resources//config.properties")) {
+            propertiesFile = new Properties();
+            propertiesFile.load(fileReader); // Load data from config.properties into propertiesFile
+            logger = LogManager.getLogger(this.getClass()); // Loads log4j2.xml file
+            switch (browser.toLowerCase()) {
+                case "chrome" -> driver = new ChromeDriver();
+                case "firefox" -> driver = new FirefoxDriver();
+                case "edge" -> driver = new EdgeDriver();
+                default -> {
+                    logger.error("Invalid browser name");
+                    return;
+                }
+            }
+            driver.manage().deleteAllCookies();
+            driver.manage().window().maximize();
+            driver.get(propertiesFile.getProperty("homepageURL"));
+        } catch (IOException e) {
+            logger.error("Properties file could not be located");
         }
-        driver.manage().deleteAllCookies();
-        driver.manage().window().maximize();
-        driver.get("https://tutorialsninja.com/demo/");
     }
 
     @AfterClass
