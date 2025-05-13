fdcc7e63ed2528ca4de1885f9c2faf70e1852490
zohairawan <35776095+zohairawan@users.noreply.github.com>
2025-05-13 01:14:58 -0700

    Add ability to run in Grid Environment

--- src/test/java/tests/base/BaseTest.java
+++ src/test/java/tests/base/BaseTest.java
@@ -5,11 +5,14 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
@@ -17,6 +20,7 @@ import org.testng.annotations.Parameters;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Properties;
@@ -34,15 +38,44 @@ public class BaseTest {
             propertiesFile = new Properties();
             propertiesFile.load(fileReader); // Load data from config.properties into propertiesFile
             logger = LogManager.getLogger(this.getClass()); // Loads log4j2.xml file
-            switch (browser.toLowerCase()) {
-                case "chrome" -> driver = new ChromeDriver();
-                case "firefox" -> driver = new FirefoxDriver();
-                case "edge" -> driver = new EdgeDriver();
-                default -> {
-                    logger.error("Invalid browser name");
-                    return;
+
+            if (propertiesFile.getProperty("executionEnv").equalsIgnoreCase("local")) {
+                switch (browser.toLowerCase()) {
+                    case "chrome" -> driver = new ChromeDriver();
+                    case "firefox" -> driver = new FirefoxDriver();
+                    case "edge" -> driver = new EdgeDriver();
+                    default -> {
+                        logger.error("Invalid browser name");
+                        return;
+                    }
                 }
             }
+
+            // Run in Grid Environment
+            if (propertiesFile.getProperty("executionEnv").equalsIgnoreCase("remote")) {
+                String hubURL = "http://localhost:4444/wd/hub";
+                DesiredCapabilities cap = new DesiredCapabilities();
+
+                switch (os.toLowerCase()) {
+                    case "windows" -> cap.setPlatform(Platform.WIN11);
+                    case "mac" -> cap.setPlatform(Platform.MAC);
+                    default -> {
+                        logger.error("No matching OS");
+                        return;
+                    }
+                }
+                switch (browser.toLowerCase()) {
+                    case "chrome" -> cap.setBrowserName("chrome");
+                    case "firefox" -> cap.setBrowserName("firefox");
+                    case "edge" -> cap.setBrowserName("MicrosoftEdge");
+                    default -> {
+                        logger.error("No matching browser");
+                        return;
+                    }
+                }
+                driver = new RemoteWebDriver(new URL(hubURL), cap);
+            }
+
             driver.manage().deleteAllCookies();
             driver.manage().window().maximize();
             driver.get(propertiesFile.getProperty("homepageURL"));
@@ -64,7 +97,7 @@ public class BaseTest {
         File targetFile = new File(targetFilePath);
         System.out.println("Target File Path: " + targetFilePath);
 //        sourceFile.renameTo(targetFile);
-        FileUtils.copyFile(sourceFile,targetFile);
+        FileUtils.copyFile(sourceFile, targetFile);
         return targetFilePath;
     }
 
